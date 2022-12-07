b5e0fb39382e5421e509deb1b525166659c424db
Emmanuel Legris <emmanuel.legris@innocap.com>
2022-12-07 13:28:10 -0500

    test

--- scripts/Restart-AzdoAgents.ps1
+++ scripts/Restart-AzdoAgents.ps1
@@ -23,6 +23,10 @@ param (
 
     [Parameter()]
     [string]
+    $subscriptionId,
+
+    [Parameter()]
+    [string]
     $tenantId,
 
     [Parameter()]
@@ -36,66 +40,49 @@ param (
 
 $env:AZURE_DEVOPS_EXT_PAT = $azdoToken
 
-
+# ----------------------------------------------
+# Azure Authentication
+# ----------------------------------------------
 
 try {
-    # ----------------------------------------------
-    # Azure Authentication
-    # ----------------------------------------------
-
-    Write-Host "Login to Azure..." -ForegroundColor Green
+    Write-Host "##[group] Azure Authentication"
+    Write-Host "##[section] Login to Azure Portal..."
     az login --service-principal -u $appId -p $secretId --tenant $tenantId
-    Write-Host "Login to Azure DevOps..." -ForegroundColor Green
-    Write-Host "Using $azdoUrl" -ForegroundColor Green
+    az account set -s $subscriptionId
+    Write-Host "##[section] Login to Azure DevOps..."
+    Write-Host "##[section] Using $azdoUrl"
     az devops configure --defaults organization=$azdoUrl
-    
-    # --------------------------------------------------------------
-    # Get Agent Status and restart containers when no jobs running
-    # --------------------------------------------------------------
-    
-    Write-Host "Starting container restart..." -ForegroundColor Green
-
-    $poolIds = @{
-        "12" = "centralus"
-        "13" = "eastus2"
-        "14" = "centralindia"
-    }
-
-    # test temp
-    $test = (az pipelines agent list --include-assigned-request --pool-id 12 --query "[].assignedRequest")
-
-    while ($test -ne "[]") {
-        Write-Host "The pool still running jobs, retrying in 10 seconds..."
-        Start-Sleep 10
-        $list = (az pipelines agent list --include-assigned-request --pool-id 12 --query "[].assignedRequest")
-    }
-
-    az container list # --query "[?location == 'centralus'].name" --output tsv
-
-    Write-Host $aciList
-
-    Write-Host "Continue after test"
-
-    # END TEST
-    
-    $poolIds.keys | ForEach-Object {
-    
-        $list = (az pipelines agent list --include-assigned-request --pool-id $_ --query "[].assignedRequest")
-    
-        while ($list -ne "[]") {
-            Write-Host "The pool in $($poolIds[$_]) still running jobs, retrying in 10 seconds..."
-            Start-Sleep 10
-            $list = (az pipelines agent list --include-assigned-request --pool-id $_ --query "[].assignedRequest")
-        }
-    
-        $aciList = (az container list --query "[?location == '$($poolIds[$_])'].name" --output tsv)
-    
-        $aciList | ForEach-Object {
-            $rg = (az container list --query "[?name == '$_'].resourceGroup" --output tsv)
-            az container restart -n $_ -g $rg --no-wait
-        }
-    }
 }
 catch {
-    Write-Warning "Azure Authentication Failed"
+    Write-Warning "##[error] Azure Authentication Failed"
+}
+
+# --------------------------------------------------------------
+# Get Agent Status and restart containers when no jobs running
+# --------------------------------------------------------------
+    
+Write-Host "##[group] Starting container restart..."
+
+$poolIds = @{
+    "12" = "centralus"
+    "13" = "eastus2"
+    "14" = "centralindia"
+}
+    
+$poolIds.keys | ForEach-Object {
+    
+    $list = (az pipelines agent list --include-assigned-request --pool-id $_ --query "[].assignedRequest")
+    
+    while ($list -ne "[]") {
+        Write-Host "##[section] The pool in $($poolIds[$_]) still running jobs, retrying in 10 seconds..."
+        Start-Sleep 10
+        $list = (az pipelines agent list --include-assigned-request --pool-id $_ --query "[].assignedRequest")
+    }
+    
+    $aciList = (az container list --query "[?location == '$($poolIds[$_])'].name" --output tsv)
+    
+    $aciList | ForEach-Object {
+        $rg = (az container list --query "[?name == '$_'].resourceGroup" --output tsv)
+        az container restart -n $_ -g $rg --no-wait
+    }
 }
