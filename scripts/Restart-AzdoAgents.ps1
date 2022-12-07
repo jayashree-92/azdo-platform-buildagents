bf4affb9cfae00ce5b3c6dca023e57b6daa5cc8c
Emmanuel Legris <emmanuel.legris@innocap.com>
2022-12-07 11:15:32 -0500

    Scripts and pipelines

---
+++ scripts/Restart-AzdoAgents.ps1
@@ -0,0 +1,66 @@
+<#
+.SYNOPSIS
+  Authenticate in Azure to return Azure DevOps Agents status to restart containers when no jobs are running.
+.DESCRIPTION
+  Authenticate in Azure to return Azure DevOps Agents status to restart containers when no jobs are running.
+  PoolIds must match with correct region to work.
+.NOTES
+  Version:          1.0
+  Purpose/Change:   Initial script development
+  Requirements:     PowerShell 7
+  Purpose:          Automation
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $appId,
+
+    [Parameter()]
+    [securestring]
+    $secretId,
+
+    [Parameter()]
+    [string]
+    $tenantId
+)
+
+# ----------------------------------------------
+# Azure Authentication
+# ----------------------------------------------
+
+try {
+    az login --service-principal -u $appId -p $secretId --tenant $tenantId
+}
+catch {
+    Write-Warning "Azure Authentication Failed"
+}
+
+$poolIds = @{
+    "12" = "centralus"
+    "13" = "eastus2"
+    "14" = "centralindia"
+}
+
+# --------------------------------------------------------------
+# Get Agent Status and restart containers when no jobs running
+# --------------------------------------------------------------
+
+$poolIds.keys | ForEach-Object {
+
+    $list = (az pipelines agent list --include-assigned-request --pool-id $_ --query "[].assignedRequest")
+
+    while ($list -ne "[]") {
+        Write-Host "The pool in $($poolIds[$_]) still running jobs, retrying in 10 seconds..."
+        Start-Sleep 10
+        $list = (az pipelines agent list --include-assigned-request --pool-id $_ --query "[].assignedRequest")
+    }
+
+    $aciList = (az container list --query "[?location == '$($poolIds[$_])'].name" --output tsv)
+
+    $aciList | ForEach-Object {
+        $rg = (az container list --query "[?name == '$_'].resourceGroup" --output tsv)
+        az container restart -n $_ -g $rg --no-wait
+    }
+}
